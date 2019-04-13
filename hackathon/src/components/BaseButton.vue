7a4e565fc1795fb3a39c21e4a6b0292800329f54
maciejkaczorowski <maciej.kaczorowski@strix.net>
2019-04-13 11:30:04 +0200

    [#2.1/2] Added BaseButton component and BaseInput with custom v-model

---
+++ hackathon/src/components/BaseButton.vue
@@ -0,0 +1,33 @@
+<template>
+    <button
+        :disabled="isDisabled"
+        :class="[{ 'btn--loading': isLoading, 'btn--disabled': isDisabled }]">
+        {{ title }}
+        <slot />
+    </button>
+</template>
+
+<script>
+export default {
+    name: "BaseButton",
+    props: {
+        title: {
+            type: String,
+            required: false,
+            default: '',
+        },
+        isLoading: {
+            type: Boolean,
+            required: true,
+        },
+        isDisabled: {
+            type: Boolean,
+            required: true,
+        },
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ hackathon/src/components/BaseInput.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="input">
+        <label v-if="label">{{ label }}</label>
+        <input
+            :value="value"
+            @input="onChange">
+    </div>
+</template>
+
+<script>
+export default {
+    name: "BaseInput",
+    props: {
+        value: {
+            type: [String, Number],
+            required: true,
+        },
+        label: {
+            type: String,
+            required: false,
+            default: null,
+        },
+    },
+    methods: {
+        onChange(event) {
+            this.$emit('input', event.target.value);
+        },
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
