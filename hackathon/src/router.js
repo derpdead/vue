49bd6d7fc8eb74a727e6851415612d1ade0dcd76
maciejkaczorowski <maciej.kaczorowski@strix.net>
2019-04-13 10:43:06 +0200

    [#1.4] Refactor

--- hackathon/src/router.js
+++ hackathon/src/router.js
@@ -32,7 +32,7 @@ const router = new Router({
 });
 
 router.beforeEach((to, from, next) => {
-    if (localStorage.isAdmin) {
+    if (localStorage.getItem('isAdmin')) {
         // TODO: Do sth...
     }
     next();
